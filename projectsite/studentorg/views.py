6f79995a128d06d6865e1d8fd94bf5ad8b8c1f16
migblasino <miguelbryson348@gmail.com>
2024-04-19 14:21:29 +0800

    updated django

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls import path
+from studentorg.views import HomePageView
+from studentorg import views
 
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', views.HomePageView.as_view(), name = 'home'),
+]
 # Create your views here.
